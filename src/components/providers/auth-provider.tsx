f56ae8eded5d34fba1f0a97ee56a56b0a90aa9d1
ThaiDuong2002 <105373894+ThaiDuong2002@users.noreply.github.com>
2024-06-20 18:08:37 +0700

    [Fix Bug]: Update 28

--- src/components/providers/auth-provider.tsx
+++ src/components/providers/auth-provider.tsx
@@ -8,7 +8,7 @@ interface Props {
 }
 
 const AuthProvider = ({ children }: Props) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return <SessionProvider basePath="/api/auth">{children}</SessionProvider>;
 };
 
 export default AuthProvider;
